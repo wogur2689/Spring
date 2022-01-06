41880ba14109d1d01728f704534ef1a9593f7658
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2022-01-06 17:02:24 +0900

    회원, 주문, 할인정책 Java구현(수정전)

--- Java구현/order/OrderServiceImpl.java
+++ Java구현/order/OrderServiceImpl.java
@@ -12,10 +12,10 @@ public class OrderServiceImpl implements OrderService {
     private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
 
     @Override
-    public Order createOrder(Long memberId, String itemNane, int itemPrice) {
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
         int discountPrice = discountPolicy.discount(member, itemPrice);
 
-        return new Order(memberId, itemNane, itemPrice, discountPrice);
+        return new Order(memberId, itemName, itemPrice, discountPrice);
     }
 }
