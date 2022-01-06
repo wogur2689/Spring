a0efae50c23a3a5fb631222add56ff1cd5bd4479
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2022-01-06 16:58:02 +0900

    회원, 주문, 할인정책 Java구현(수정전)

--- Java구현/order/OrderService.java
+++ Java구현/order/OrderService.java
@@ -1,5 +1,5 @@
 package hello.core.order;
 
 public interface OrderService {
-    Order createOrder(Long memberId, String itemNane, int itemPrice);
+    Order createOrder(Long memberId, String itemName, int itemPrice);
 }
